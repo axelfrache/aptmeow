094734bd8ae2f5a48a770fb9e4de163343b80e58
Axel Frache <axelfrache@gmail.com>
2025-06-04 13:00:27 +0200

    Update script

--- script.sh
+++ script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-docker compose exec debian-repo \
-  bash -c "mkdir -p dists/stable/main/binary-all && \
-           dpkg-scanpackages pool /dev/null | gzip -9c > dists/stable/main/binary-all/Packages.gz"
+
+echo "[+] Regenerating Packages.gz for all & amd64..."
+
+docker compose exec debian-repo bash -c "
+  mkdir -p dists/stable/main/binary-all dists/stable/main/binary-amd64 && \
+  dpkg-scanpackages pool /dev/null | tee dists/stable/main/binary-all/Packages | gzip -9c > dists/stable/main/binary-all/Packages.gz && \
+  cp dists/stable/main/binary-all/Packages dists/stable/main/binary-amd64/Packages && \
+  gzip -9c dists/stable/main/binary-amd64/Packages > dists/stable/main/binary-amd64/Packages.gz
+"
+
+echo "[✓] Packages.gz updated for all & amd64"
